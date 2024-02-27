f9d884f4f3ad68c709243ed9bd4e51caa3f2a371
3dpaulharvey <3dpaulharvey@gmail.com>
2024-02-27 13:37:03 -0800

    32131

--- mcptest1.py
+++ mcptest1.py
@@ -1,7 +1,5 @@
 # Simple example of reading the MCP3008 analog input channels and printing
 # them all out.
-# Author: Tony DiCola
-# License: Public Domain
 import time
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
@@ -9,12 +7,7 @@ import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 
-# Software SPI configuration:
-# CLK  = 18
-# MISO = 23
-# MOSI = 24
-# CS   = 25
-# mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
 
 # Hardware SPI configuration:
 SPI_PORT   = 0
